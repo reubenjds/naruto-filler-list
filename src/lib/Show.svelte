03d7d51664a64836fd2e1f308656e5d2b3b0319b
Reuben D'Souza <reubenjds@gmail.com>
2024-10-14 18:59:37 -0400

    fix: switch to SvelteKit

--- src/lib/Show.svelte
+++ src/lib/Show.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+	export let show: { name: string; image: string };
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<div class="card bg-base-100 w-96">
+	<figure>
+		<img src={show.image} alt={show.name} />
+	</figure>
+	<div class="card-body">
+		<div class="card-actions justify-center">
+			<button class="btn btn-warning">{show.name}</button>
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Show from '$lib/Show.svelte';
+
+	let shows = [
+		{
+			name: 'Naruto',
+			image: '/naruto.jpg'
+		},
+		{
+			name: 'Naruto Shippuden',
+			image: '/naruto_shippuden.jpg'
+		}
+	];
+</script>
+
+<main>
+	<div class="flex flex-col justify-center items-center min-h-screen">
+		<h1 class="text-4xl font-bold text-center py-2">Naruto Filler List</h1>
+		<div>
+			<div class="flex flex-row gap-4">
+				{#each shows as show}
+					<Show {show} />
+				{/each}
+			</div>
+		</div>
+	</div>
+</main>
