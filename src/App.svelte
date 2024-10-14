f0745ae2c0345888ad23d1cc89b10c68e86f1e2d
Reuben D'Souza <reubenjds@gmail.com>
2024-10-14 18:26:52 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<main>
+	<div class="flex flex-col justify-center items-center min-h-screen">
+		<h1 class="text-4xl font-bold text-center py-2">Naruto Filler List</h1>
+		<div>
+			<div class="flex flex-row gap-4">
+				<div class="card bg-base-100 w-96">
+					<figure>
+						<img src="/naruto.jpg" alt="Naruto 2002" />
+					</figure>
+					<div class="card-body">
+						<div class="card-actions justify-center">
+							<button class="btn btn-warning">Naruto (2002)</button>
+						</div>
+					</div>
+				</div>
+				<div class="card bg-base-100 w-96 shadow-xl">
+					<figure>
+						<img src="/naruto_shippuden.jpg" alt="Naruto: Shippuden" />
+					</figure>
+					<div class="card-body">
+						<div class="card-actions justify-center">
+							<button class="btn btn-warning">Naruto: Shippuden</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
