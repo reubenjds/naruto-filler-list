835b131252572d40d2c8e5c292b74d158bc38bb1
Reuben D'Souza <reubenjds@gmail.com>
2024-10-17 22:00:55 -0400

    fix: mobile modal css

--- src/routes/naruto/+page.svelte
+++ src/routes/naruto/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-
 	import { naruto, narutoArcs } from '$lib/episodes';
 	import Episode from '$lib/Episode.svelte';
 	import { onMount } from 'svelte';
@@ -12,6 +10,7 @@
 	let selectedEpisode = '';
 	let showTitles = true;
 	let useTable = false;
+	let modal: HTMLDialogElement;
 
 	$: {
 		if (selectedArc !== -1) {
@@ -45,22 +44,20 @@
 			/>
 			{#if isSmallScreen}
 				<!-- Modal for small screens -->
-				<input type="checkbox" id="modal-toggle" class="modal-toggle" />
-				<div class="modal">
-					<div class="modal-box relative flex flex-col gap-2">
-						<label for="modal-toggle" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-							>✕</label
-						>
+				<button class="btn join-item m-1" on:click={() => modal.showModal()}>Options</button>
+				<dialog bind:this={modal} class="modal">
+					<div class="modal-box space-y-4">
+						<h3 class="text-lg font-bold">Options</h3>
 						<select
 							bind:value={selectedArc}
-							class="select select-bordered w-full max-w-xs join-item mt-6"
+							class="select select-bordered w-full max-w-xs join-item mt-4"
 						>
 							<option value={-1} selected>All Episodes</option>
 							{#each narutoArcs as arc, index}
 								<option value={index}>{arc.name}</option>
 							{/each}
 						</select>
-						<div class="form-control">
+						<div class="form-control mt-4">
 							<label class="label cursor-pointer">
 								<span class="label-text">Show Episode Titles</span>
 								<input
@@ -70,9 +67,15 @@
 								/>
 							</label>
 						</div>
+						<button
+							class=" justify-center place-items-center badge-xs badge-error absolute right-3 top-1 transition duration-300 rounded-full"
+							on:click={() => modal.close()}>x</button
+						>
 					</div>
-				</div>
-				<label for="modal-toggle" class="btn join-item m-1">Options</label>
+					<form method="dialog" class="modal-backdrop">
+						<button>close</button>
+					</form>
+				</dialog>
 			{:else}
 				<!-- Dropdown for larger screens -->
 				<details class="dropdown">
