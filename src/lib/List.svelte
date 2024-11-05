0bb19cd97edea9ac541bf56003988130dbf8c0e8
Reuben D'Souza <reubenjds@gmail.com>
2024-11-05 13:37:23 -0500

    fix: home route css

--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -61,142 +61,81 @@
 </script>
 
 <main>
-	<input type="checkbox" id="my_modal_7" class="modal-toggle" />
-	<div class="modal" class:modal-open={$openQuickList}>
-		<div class="modal-box">
-			<div class="h-full overflow-x-auto">
-				<table class="table table-pin-rows">
-					<thead>
-						<tr>
-							<th class="text-green-500 text-base">Canon</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr><td class="text-white">{quickList.Manga_Canon_Episodes.join(', ')}</td></tr>
-					</tbody>
-					<thead>
-						<tr>
-							<th class="text-yellow-500 text-base">Mixed</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr><td class="text-white">{quickList.Mixed_Canon_Filler_Episodes.join(', ')}</td></tr>
-					</tbody>
-					<thead>
-						<tr>
-							<th class="text-red-500 text-base">Filler</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr><td class="text-white">{quickList.Filler_Episodes.join(', ')}</td></tr>
-					</tbody>
-				</table>
+	<div class="flex flex-col min-h-screen place-items-center p-5">
+		<div class="navbar bg-base-100 p-5">
+			<div class="flex-1">
+				<a class="btn btn-ghost" href="/">
+					<img src="/logo.png" alt="Logo" class="h-12 w-20" />
+				</a>
+			</div>
+			<div class="flex-none">
+				<ul class="menu menu-horizontal px-1">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+					<li on:click={() => openQuickList.update((value) => !value)}><a>Quick List</a></li>
+					<li><a>About</a></li>
+				</ul>
 			</div>
 		</div>
-		<button class="modal-backdrop" on:click={() => openQuickList.update((value) => !value)}
-			>Close</button
-		>
-	</div>
-	<div class="flex flex-col place-items-center h-full gap-4 m-6">
-		<h1 class="text-4xl font-bold text-center pt-4">{name} Filler List</h1>
 
-		<div class="join flex place-items-center">
-			<input
-				bind:value={selectedEpisode}
-				class="input input-bordered join-item"
-				placeholder="Episode"
-			/>
-			{#if isSmallScreen}
-				<!-- Modal for small screens -->
-				<input type="checkbox" id="modal-toggle" class="modal-toggle" />
-				<div class="modal">
-					<div class="modal-box relative flex flex-col gap-2">
-						<label for="modal-toggle" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-							>✕</label
-						>
-						<select bind:value={selectedArc} class="select select-bordered w-full join-item mt-6">
-							<option value={-1} selected>All Episodes</option>
-							{#each narutoArcs as arc, index}
-								<option value={index}>{arc.name}</option>
-							{/each}
-						</select>
-						<div class="form-control">
-							<label class="label cursor-pointer">
-								<span class="label-text">View as Table</span>
-								<input type="checkbox" bind:checked={useTable} class="toggle toggle-primary" />
-							</label>
-						</div>
-
-						<div class="form-control">
-							<label class="label cursor-pointer">
-								<span class="label-text">Show Titles</span>
-								<input type="checkbox" bind:checked={showTitles} class="toggle toggle-primary" />
-							</label>
-						</div>
-
-						<div class="form-control">
-							<label class="label cursor-pointer">
-								<span class="label-text">Show Descriptions</span>
-								<input
-									type="checkbox"
-									bind:checked={showExplanations}
-									class="toggle toggle-primary"
-								/>
-							</label>
-						</div>
-						<div class="form-control flex flex-row gap-2 items-start justify-center">
-							<label class="label cursor-pointer">
-								<input
-									type="radio"
-									name="episodeType"
-									value="all"
-									bind:group={selectedEpisodeType}
-									class="radio radio-info"
-								/>
-								<span class="label-text p-1">All</span>
-							</label>
-							<label class="label cursor-pointer">
-								<input
-									type="radio"
-									name="episodeType"
-									value="filler"
-									bind:group={selectedEpisodeType}
-									class="radio radio-error"
-								/>
-								<span class="label-text p-1">Filler</span>
-							</label>
-							<label class="label cursor-pointer">
-								<input
-									type="radio"
-									name="episodeType"
-									value="mixedCanonFiller"
-									bind:group={selectedEpisodeType}
-									class="radio radio-warning"
-								/>
-								<span class="label-text p-1">Mixed</span>
-							</label>
-							<label class="label cursor-pointer">
-								<input
-									type="radio"
-									name="episodeType"
-									value="canon"
-									bind:group={selectedEpisodeType}
-									class="radio radio-success"
-								/>
-								<span class="label-text p-1">Canon</span>
-							</label>
-						</div>
-					</div>
+		<input type="checkbox" id="my_modal_7" class="modal-toggle" />
+		<div class="modal" class:modal-open={$openQuickList}>
+			<div class="modal-box">
+				<div class="h-full overflow-x-auto">
+					<table class="table table-pin-rows">
+						<thead>
+							<tr>
+								<th class="text-green-500 text-base">Canon</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td class="text-white">{quickList.Manga_Canon_Episodes.join(', ')}</td></tr>
+						</tbody>
+						<thead>
+							<tr>
+								<th class="text-yellow-500 text-base">Mixed</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td class="text-white">{quickList.Mixed_Canon_Filler_Episodes.join(', ')}</td></tr
+							>
+						</tbody>
+						<thead>
+							<tr>
+								<th class="text-red-500 text-base">Filler</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td class="text-white">{quickList.Filler_Episodes.join(', ')}</td></tr>
+						</tbody>
+					</table>
 				</div>
-				<label for="modal-toggle" class="btn join-item m-1">Options</label>
-			{:else}
-				<!-- Dropdown for larger screens -->
-				<details class="dropdown">
-					<summary class="btn join-item m-1">Options</summary>
-					<ul class="dropdown-content menu bg-base-100 rounded-box z-[99] w-max p-2 shadow">
-						<div class="flex flex-col gap-1 p-2">
-							<select bind:value={selectedArc} class="select select-bordered w-full join-item">
-								<option value={-1} selected>All Arcs</option>
+			</div>
+			<button class="modal-backdrop" on:click={() => openQuickList.update((value) => !value)}
+				>Close</button
+			>
+		</div>
+		<div class="flex flex-col place-items-center h-full gap-4 m-6">
+			<h1 class="text-4xl font-bold text-center pt-4">{name} Filler List</h1>
+
+			<div class="join flex place-items-center">
+				<input
+					bind:value={selectedEpisode}
+					class="input input-bordered join-item"
+					placeholder="Episode"
+				/>
+				{#if isSmallScreen}
+					<!-- Modal for small screens -->
+					<input type="checkbox" id="modal-toggle" class="modal-toggle" />
+					<div class="modal">
+						<div class="modal-box relative flex flex-col gap-2">
+							<label
+								for="modal-toggle"
+								class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label
+							>
+							<select bind:value={selectedArc} class="select select-bordered w-full join-item mt-6">
+								<option value={-1} selected>All Episodes</option>
 								{#each narutoArcs as arc, index}
 									<option value={index}>{arc.name}</option>
 								{/each}
@@ -254,7 +193,7 @@
 										bind:group={selectedEpisodeType}
 										class="radio radio-warning"
 									/>
-									<span class="label-text p-1">Mixed Canon/Filler</span>
+									<span class="label-text p-1">Mixed</span>
 								</label>
 								<label class="label cursor-pointer">
 									<input
@@ -268,53 +207,164 @@
 								</label>
 							</div>
 						</div>
-					</ul>
-				</details>
-			{/if}
-		</div>
-		{#key Episodes}
-			{#if useTable}
-				<table class="table table-compact table-zebra place-content-center justify-center">
-					<thead>
-						<tr>
-							<th class="md:text-base text-center">Episode</th>
-							<th class="md:text-base">Title</th>
-							<th class="md:text-base text-center">Type</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each Episodes as episode}
+					</div>
+					<label for="modal-toggle" class="btn join-item m-1">Options</label>
+				{:else}
+					<!-- Dropdown for larger screens -->
+					<details class="dropdown">
+						<summary class="btn join-item m-1">Options</summary>
+						<ul class="dropdown-content menu bg-base-100 rounded-box z-[99] w-max p-2 shadow">
+							<div class="flex flex-col gap-1 p-2">
+								<select bind:value={selectedArc} class="select select-bordered w-full join-item">
+									<option value={-1} selected>All Arcs</option>
+									{#each narutoArcs as arc, index}
+										<option value={index}>{arc.name}</option>
+									{/each}
+								</select>
+								<div class="form-control">
+									<label class="label cursor-pointer">
+										<span class="label-text">View as Table</span>
+										<input type="checkbox" bind:checked={useTable} class="toggle toggle-primary" />
+									</label>
+								</div>
+
+								<div class="form-control">
+									<label class="label cursor-pointer">
+										<span class="label-text">Show Titles</span>
+										<input
+											type="checkbox"
+											bind:checked={showTitles}
+											class="toggle toggle-primary"
+										/>
+									</label>
+								</div>
+
+								<div class="form-control">
+									<label class="label cursor-pointer">
+										<span class="label-text">Show Descriptions</span>
+										<input
+											type="checkbox"
+											bind:checked={showExplanations}
+											class="toggle toggle-primary"
+										/>
+									</label>
+								</div>
+								<div class="form-control flex flex-row gap-2 items-start justify-center">
+									<label class="label cursor-pointer">
+										<input
+											type="radio"
+											name="episodeType"
+											value="all"
+											bind:group={selectedEpisodeType}
+											class="radio radio-info"
+										/>
+										<span class="label-text p-1">All</span>
+									</label>
+									<label class="label cursor-pointer">
+										<input
+											type="radio"
+											name="episodeType"
+											value="filler"
+											bind:group={selectedEpisodeType}
+											class="radio radio-error"
+										/>
+										<span class="label-text p-1">Filler</span>
+									</label>
+									<label class="label cursor-pointer">
+										<input
+											type="radio"
+											name="episodeType"
+											value="mixedCanonFiller"
+											bind:group={selectedEpisodeType}
+											class="radio radio-warning"
+										/>
+										<span class="label-text p-1">Mixed Canon/Filler</span>
+									</label>
+									<label class="label cursor-pointer">
+										<input
+											type="radio"
+											name="episodeType"
+											value="canon"
+											bind:group={selectedEpisodeType}
+											class="radio radio-success"
+										/>
+										<span class="label-text p-1">Canon</span>
+									</label>
+								</div>
+							</div>
+						</ul>
+					</details>
+				{/if}
+			</div>
+			{#key Episodes}
+				{#if useTable}
+					<table class="table table-compact table-zebra place-content-center justify-center">
+						<thead>
 							<tr>
-								<td class="md:text-base text-center">{episode.number}</td>
-								<td class={showTitles ? 'md:text-base' : 'md:text-base text-transparent'}
-									>{episode.title}</td
-								>
-								{#if episode.type === 'Filler' || (episode.explanation ?? '') !== ''}
-									<td class="md:text-base text-center">
-										{#if isSmallScreen}
-											<input
-												type="checkbox"
-												id="modal-toggle-{episode.number}"
-												class="modal-toggle"
-											/>
-											<div class="modal">
-												<div class="modal-box relative">
+								<th class="md:text-base text-center">Episode</th>
+								<th class="md:text-base">Title</th>
+								<th class="md:text-base text-center">Type</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#each Episodes as episode}
+								<tr>
+									<td class="md:text-base text-center">{episode.number}</td>
+									<td class={showTitles ? 'md:text-base' : 'md:text-base text-transparent'}
+										>{episode.title}</td
+									>
+									{#if episode.type === 'Filler' || (episode.explanation ?? '') !== ''}
+										<td class="md:text-base text-center">
+											{#if isSmallScreen}
+												<input
+													type="checkbox"
+													id="modal-toggle-{episode.number}"
+													class="modal-toggle"
+												/>
+												<div class="modal">
+													<div class="modal-box relative">
+														<label
+															for="modal-toggle-{episode.number}"
+															class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+															>✕</label
+														>
+														<h3 class="text-lg font-bold">
+															Episode {episode.number} - {episode.type}
+														</h3>
+														<p class="py-4">{episode.explanation ?? ''}</p>
+													</div>
+												</div>
+												{#if showExplanations}
 													<label
 														for="modal-toggle-{episode.number}"
-														class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label
+														class="badge {episode.type === 'Filler'
+															? 'badge-error'
+															: 'badge-warning'} text-xs sm:text-base h-fit cursor-pointer relative"
 													>
-													<h3 class="text-lg font-bold">
-														Episode {episode.number} - {episode.type}
-													</h3>
-													<p class="py-4">{episode.explanation ?? ''}</p>
-												</div>
-											</div>
-											{#if showExplanations}
-												<label
-													for="modal-toggle-{episode.number}"
+														{episode.type}
+														<span
+															class="absolute top-0 right-0 text-xs indicator-item badge badge-primary px-1 {!showExplanations
+																? 'hidden'
+																: ''}"
+															style="transform: translate(50%, -50%);">?</span
+														>
+													</label>
+												{:else}
+													<!-- svelte-ignore a11y-label-has-associated-control -->
+													<label
+														class="badge {episode.type === 'Filler'
+															? 'badge-error'
+															: 'badge-warning'} text-xs sm:text-base h-fit relative"
+													>
+														{episode.type}
+													</label>
+												{/if}
+											{:else}
+												<span
 													class="badge {episode.type === 'Filler'
 														? 'badge-error'
-														: 'badge-warning'} text-xs sm:text-base h-fit cursor-pointer relative"
+														: 'badge-warning'} text-xs sm:text-base h-fit cursor-pointer tooltip tooltip-left relative"
+													data-tip={showExplanations ? episode.explanation : ''}
 												>
 													{episode.type}
 													<span
@@ -323,59 +373,35 @@
 															: ''}"
 														style="transform: translate(50%, -50%);">?</span
 													>
-												</label>
-											{:else}
-												<!-- svelte-ignore a11y-label-has-associated-control -->
-												<label
-													class="badge {episode.type === 'Filler'
-														? 'badge-error'
-														: 'badge-warning'} text-xs sm:text-base h-fit relative"
-												>
-													{episode.type}
-												</label>
+												</span>
 											{/if}
-										{:else}
-											<span
-												class="badge {episode.type === 'Filler'
-													? 'badge-error'
-													: 'badge-warning'} text-xs sm:text-base h-fit cursor-pointer tooltip tooltip-left relative"
-												data-tip={showExplanations ? episode.explanation : ''}
+										</td>
+									{:else if episode.type === 'Mixed Canon/Filler'}
+										<td class="md:text-base text-center">
+											<span class="badge badge-warning text-xs sm:text-base h-fit"
+												>Mixed Canon/Filler</span
 											>
-												{episode.type}
-												<span
-													class="absolute top-0 right-0 text-xs indicator-item badge badge-primary px-1 {!showExplanations
-														? 'hidden'
-														: ''}"
-													style="transform: translate(50%, -50%);">?</span
-												>
-											</span>
-										{/if}
-									</td>
-								{:else if episode.type === 'Mixed Canon/Filler'}
-									<td class="md:text-base text-center">
-										<span class="badge badge-warning text-xs sm:text-base h-fit"
-											>Mixed Canon/Filler</span
-										>
-									</td>
-								{:else}
-									<td class="md:text-base text-center">
-										<span class="badge badge-success text-xs sm:text-base h-fit">Canon</span>
-									</td>
-								{/if}
-							</tr>
+										</td>
+									{:else}
+										<td class="md:text-base text-center">
+											<span class="badge badge-success text-xs sm:text-base h-fit">Canon</span>
+										</td>
+									{/if}
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				{:else}
+					<div
+						class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+						style="width: 100%; overflow-x: auto;"
+					>
+						{#each Episodes as episode}
+							<Ep {episode} {showTitles} {showExplanations} />
 						{/each}
-					</tbody>
-				</table>
-			{:else}
-				<div
-					class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
-					style="width: 100%; overflow-x: auto;"
-				>
-					{#each Episodes as episode}
-						<Ep {episode} {showTitles} {showExplanations} />
-					{/each}
-				</div>
-			{/if}
-		{/key}
+					</div>
+				{/if}
+			{/key}
+		</div>
 	</div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Show from '$lib/Show.svelte';
-	import openWelcomeModal from './+layout.svelte';
+	import { openQuickList } from '$lib/store.ts';
 
 	let shows = [
 		{
@@ -17,14 +17,52 @@
 </script>
 
 <main>
-	<div class="flex flex-col justify-center place-items-center">
-		<!-- <h1 class="text-4xl font-bold text-center py-2">Naruto Filler List</h1> -->
-		<div>
-			<div class="flex flex-col sm:flex-row gap-12">
-				{#each shows as show}
-					<Show {show} />
-				{/each}
+	<div class="flex flex-col min-h-screen place-items-center p-5 place-content-between">
+		<div class="navbar bg-base-100 p-5">
+			<div class="flex-1">
+				<a class="btn btn-ghost" href="/">
+					<img src="/logo.png" alt="Logo" class="h-12 w-20" />
+				</a>
+			</div>
+			<div class="flex-none">
+				<ul class="menu menu-horizontal px-1">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+					<li on:click={() => openQuickList.update((value) => !value)}><a>Quick List</a></li>
+					<li><a>About</a></li>
+				</ul>
 			</div>
 		</div>
+
+		<div class="flex flex-col justify-center place-items-center">
+			<!-- <h1 class="text-4xl font-bold text-center py-2">Naruto Filler List</h1> -->
+			<div>
+				<div class="flex flex-col sm:flex-row gap-12">
+					{#each shows as show}
+						<Show {show} />
+					{/each}
+				</div>
+			</div>
+		</div>
+		<footer
+			class="footer footer-center bg-base-300 text-base-content p-4 w-1/2 min-w-fit rounded-full"
+		>
+			<aside>
+				<p class="flex items-center justify-center gap-2 text-lg">
+					Hosted with ❤️ on <a href="https://github.com/reubenjds/naruto-filler-list"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							class="inline w-6 h-6"
+							fill="currentColor"
+							><path
+								d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.11.82-.26.82-.577 0-.285-.01-1.04-.015-2.04-3.338.725-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.805 1.305 3.49.997.108-.775.418-1.305.76-1.605-2.665-.305-5.467-1.335-5.467-5.93 0-1.31.47-2.38 1.235-3.22-.125-.305-.535-1.53.115-3.18 0 0 1.005-.32 3.3 1.23.955-.265 1.98-.397 3-.403 1.02.006 2.045.138 3 .403 2.28-1.55 3.285-1.23 3.285-1.23.655 1.65.245 2.875.12 3.18.77.84 1.235 1.91 1.235 3.22 0 4.61-2.805 5.62-5.475 5.92.43.37.81 1.1.81 2.22 0 1.605-.015 2.895-.015 3.285 0 .32.215.695.825.575C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"
+							/></svg
+						></a
+					>
+				</p>
+			</aside>
+		</footer>
 	</div>
 </main>
