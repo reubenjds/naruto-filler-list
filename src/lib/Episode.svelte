19f1714669dd834e778f43f185b24fbc723852bd
spencerhum04 <spencerhum04@gmail.com>
2024-11-04 21:54:07 -0500

    fix: better card css

--- src/lib/Episode.svelte
+++ src/lib/Episode.svelte
@@ -17,35 +17,29 @@
 
 <div>
 	<div
-		class="card w-full min-w-[20%] bg-base-300 shadow-xl image-full min-h-[20rem] transition-all duration-300 ease-in-out"
+		class="card w-full min-w-[20%] md:min-h-88 lg:min-h-72 xl:min-h-80 bg-base-300 shadow-xl image-full min-h-[15rem] transition-all duration-300 ease-in-out "
 		style="background-color: {epColour()};"
 	>
-		<div class="card-body p-4 md:p-6 lg:p-8">
-			{#if showTitles}
-				<div class="absolute top-4 right-4 bg-base-500 text-white md:pl-2 rounded-bl-lg">
-					<p class="text-xs md:text-sm">{episode.number}</p>
-				</div>
-				<h2 class="pb-3 pt-6 md:pt-1 text-base md:text-xl lg:text-2xl">
-					{episode.title}
-				</h2>
-				<p class="line-clamp-5 whitespace-pre-line italic text-sm md:text-base lg:text-lg">
-					{showExplanations ? (episode.explanation ?? '') : ''}
-				</p>
-				<div class="flex justify-between items-center">
-					<p class="text-xs md:text-sm lg:text-base bg-base-500">{episode.type}</p>
-				</div>
-			{:else}
+		<div class="card-body p-6">
+			{#if !(showExplanations || showTitles)}
 				<div
 					class="absolute inset-0 flex items-center justify-center bg-base-500 text-white md:pl-2 rounded-bl-lg"
 				>
-					<p class="flex place-items-center justify-center text-5xl">{episode.number}</p>
+					<p class="flex place-items-center justify-center font-semibold text-6xl text-slate-200">{episode.number}</p>
 				</div>
-
-				<p class="line-clamp-5 whitespace-pre-line italic text-sm md:text-base lg:text-lg">
-					{showExplanations ? (episode.explanation ?? '') : ''}
+				<div class="flex justify-between items-center">
+					<p class="text-sm md:text-sm lg:text-base 2xl:text-lg bg-base-500 text-center uppercase font-mono text-slate-300">{episode.type}</p>
+				</div>
+			{:else}
+				<div class="flex flex-row items-center gap-x-5 text-slate-200">
+					<div class="font-semibold text-4xl 2xl:text-6xl">{episode.number}</div>
+					<div class="text-xs md:text-sm lg:text-sm 2xl:text-sm font-medium">{showTitles ? episode.title : ''}</div>
+				</div>
+				<p class="line-clamp-5 whitespace-pre-line mt-1 text-slate-200 text-xs md:text-sm lg:text-sm">
+					{showExplanations ? episode.explanation ?? '' : ''}
 				</p>
 				<div class="flex justify-between items-center">
-					<p class="text-xs md:text-sm lg:text-base bg-base-500">{episode.type}</p>
+					<p class="text-sm md:text-sm lg:text-base 2xl:text-lg bg-base-500 text-center uppercase font-mono text-slate-300">{episode.type}</p>
 				</div>
 			{/if}
 		</div>
